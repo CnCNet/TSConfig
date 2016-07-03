b9c8abbe273e8787b88efed988c9e234d115524f
User <User@COMPUTER>
2016-07-03 01:49:30 -0400

    Changed 'Visible' to 'false' for 'ONlyRightClickDeselect'.
    This feature is not yet perfect, but other code left to easy re-enable
    when it is.

--- gui/SetupFrame.Designer.cs
+++ gui/SetupFrame.Designer.cs
@@ -613,6 +613,7 @@
             this.chkUseOnlyRightClickDeselect.TabIndex = 72;
             this.chkUseOnlyRightClickDeselect.Text = " ";
             this.chkUseOnlyRightClickDeselect.UseVisualStyleBackColor = true;
+            this.chkUseOnlyRightClickDeselect.Visible = false;
             // 
             // lblUseOnlyRightClickDeselect
             // 
@@ -621,6 +622,7 @@
             this.lblUseOnlyRightClickDeselect.Size = new System.Drawing.Size(145, 14);
             this.lblUseOnlyRightClickDeselect.TabIndex = 73;
             this.lblUseOnlyRightClickDeselect.Text = "Force Right-Click De-Select:";
+            this.lblUseOnlyRightClickDeselect.Visible = false;
             // 
             // chkUseCustomColors
             // 
